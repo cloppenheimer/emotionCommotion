ac17e728177ab89fed01d9e6546e8e06b40639a2
Clara <cloppenheimer@gmail.com>
2017-12-09 21:36:37 -0500

    adding

---
+++ server.js
@@ -0,0 +1,40 @@
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
+var path = require('path');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+	response.sendFile(path.resolve('start.html'));
+});
+
+app.get('/game.html', function(request, response) {
+	response.sendFile(path.resolve('game.html'));
+});
+
+app.post('/submit', function(request, response){
+	response.send({"vehicles":[],"passengers":[]});
+});
+
+app.listen(app.get('port'), function() {
+  //console.log('Node app is running on port', app.get('port'));
+});
+
+//app.listen(process.env.PORT || 5000);
